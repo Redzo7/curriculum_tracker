100015b3967589d73b3ee9400af00724a7f4aefb
Rezso Hosi <rezsohosi@gmail.com>
2026-01-18 15:40:29 +0100

    Timestamps and course details added

--- app/db/prerequisite.ts
+++ app/db/prerequisite.ts
@@ -1,14 +1,16 @@
-import { primaryKey, integer, pgEnum, pgTable, text } from "drizzle-orm/pg-core";
+import { primaryKey, pgEnum, pgTable, text } from "drizzle-orm/pg-core";
 import { courses } from "./course";
+import { timestamps } from "./columns.helpers";
 
 const prerequisiteType = pgEnum("prereq_type", ["weak", "strong"]);
 
 export const coursePrerequisite = pgTable(
     "course_prerequisite",
     {
-        "courseId": text("course_id").references(() => courses.code),
-        "prerequisiteId": text("prereq_id").references(() => courses.code),
-        "type": prerequisiteType().default("strong")
+        "courseId": text("course_id").references(() => courses.code, {onDelete: "cascade"}).notNull(),
+        "prerequisiteId": text("prereq_id").references(() => courses.code, {onDelete: "cascade"}).notNull(),
+        "type": prerequisiteType("type").default("strong"),
+        ...timestamps
     },
     (table) => [
         primaryKey({columns: [table.courseId, table.prerequisiteId]})
